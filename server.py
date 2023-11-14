c679cd76a6024c29fe7cd2f722029ffc1192903c
Mali Moore <jamaltmoore@outlook.com>
2023-11-14 08:34:42 -0500

    .

---
+++ server.py
@@ -0,0 +1,11 @@
+import http.server, ssl
+
+server_address = ('127.0.0.1', 50000)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='cert.pem',
+                               keyfile='key.pem',
+                               ssl_version=ssl.PROTOCOL_TLS)
+
+httpd.serve_forever()
