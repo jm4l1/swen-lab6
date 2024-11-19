a22cfeb046e880f2d3f747cd18c1ce496ceba976
Mali Moore <jamaltmoore@outlook.com>
2024-11-19 00:05:19 -0500

    - minor change

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-import http.server, ssl
+import http.server
+import ssl
 
-server_address = ('127.0.0.1', 50000)
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+server_address = ('swen2003.example.com', 50000)
+httpd = http.server.HTTPServer(
+    server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
                                certfile='cert.pem',
